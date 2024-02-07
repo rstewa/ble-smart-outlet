94ffc4d7400c622a4ea8380cc15ba1804e670680
Ryan Stewart <ryan_stewart@trimble.com>
2024-02-07 17:46:16 -0600

    wip

--- ble-smart-outlet/ble-smart-outlet.ino
+++ ble-smart-outlet/ble-smart-outlet.ino
@@ -1,86 +1,106 @@
-//server
+/* Author: Ryan Stewart
+ * Date: 2/7/24
+*/
+
+#include <BLE2902.h> // this include is needed to force the compiler to find the ESP32 BLE library instead of the ArduinoBLE library
 #include <BLEDevice.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
 #include <BLEServer.h>
-#include <Wire.h>
-#include <Digital_Light_TSL2561.h>
-#include <Arduino.h>
-#include <U8x8lib.h>
-#include <Wire.h>
 
-// OLEDs without Reset of the Display
-// U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+#define SERVICE_NAME "BLE Smart Outlet"
+#define SERVICE_UUID "bfa7820b-1d87-49ed-8908-82b0e347ca56"
+#define CHARACTERISTIC_UUID "9ffa81aa-bb6e-4254-8f3a-94b0154eea50"
 
-#define bleServerName "ble-smart-outlet"
+const uint8_t STATE_OFF = 0;
+const uint8_t STATE_ON = 1;
+const int relayPin = 7;
 
-BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
+uint8_t state = STATE_OFF;
+int deviceConnected = false;
 
-int state = -1;
-int pinout = 9;
-
-enum {
-  STATE_OFF = 0,
-  STATE_ON = 1,
-};
-
-class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
     deviceConnected = true;
+    Serial.println("Connected to a central device.");
   };
-  
-  void onDisconnect(BLEServer* pServer) {
+
+  void onDisconnect(BLEServer *pServer)
+  {
     deviceConnected = false;
+    Serial.println("Disconnected from a central device.");
+    delay(500);                  // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
   }
 };
 
-void setup() {
-  Serial.begin(115200);
-  
-  //OLED display setup
-  // u8x8.begin();
-  // u8x8.setFlipMode(1);
+class StateCharacteristicCallback : public BLECharacteristicCallbacks
+{
+    void onWrite(BLECharacteristic *pCharacteristic)
+    {
+        std::string tmp = pCharacteristic->getValue();
+        const uint8_t *value = reinterpret_cast<const uint8_t*>(tmp.c_str());
+        
+        if (memcmp(value, &STATE_OFF, 1) == 0)
+        {
+            state = STATE_OFF;
+            digitalWrite(relayPin, LOW);
+            Serial.println("Turning off");
+        }
+        else if (memcmp(value, &STATE_ON, 1) == 0)
+        {
+            state = STATE_ON;
+            digitalWrite(relayPin, HIGH);
+            Serial.println("Turning on");
+        }
+        else
+        {
+            Serial.println("Invalid value");
+        }
+    }
+};
 
-  // u8x8.setFont(u8x8_font_chroma48medium8_r);
-  // u8x8.drawString(0, 3, "Starting...");
+void setup()
+{
+    Serial.begin(115200);
 
-  // Wire.begin();
-  // TSL2561.init();
-  
-  BLEDevice::init(bleServerName);
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-  
-  BLEService *pService = pServer->createService(BLEUUID((uint16_t)0x181A));
-  pCharacteristic = pService->createCharacteristic(
-    BLEUUID((uint16_t)0x2A59), // Analog Output
-    BLECharacteristic::PROPERTY_NOTIFY
-  );
-  pCharacteristic->addDescriptor(new BLE2902());
-  
-  pService->start();
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(pService->getUUID());
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x0);
-  pAdvertising->setMinPreferred(0x1F);
-  BLEDevice::startAdvertising();
-  u8x8.clearDisplay();
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(relayPin, OUTPUT);
+
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(relayPin, LOW);
+
+    // create BLE server
+
+    BLEDevice::init(SERVICE_NAME);
+    BLEServer *pServer = BLEDevice::createServer();
+
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    BLECharacteristic *stateCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+    stateCharacteristic->setCallbacks(new StateCharacteristicCallback());
+
+    stateCharacteristic->setValue(&state, 1);
+    pService->start();
+
+    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    pAdvertising->start();
 }
 
-void loop() {
-  if (deviceConnected) {
-    // light_val = TSL2561.readVisibleLux();
-    pCharacteristic->setValue(light_val);
-    pCharacteristic->notify();
-    u8x8.setFont(u8x8_font_chroma48medium8_r);
-    u8x8.setCursor(0, 0);
-    u8x8.print("Light Value:");
-    u8x8.clearLine(2);
-    u8x8.setCursor(0, 2);
-    u8x8.print(light_val);
-    u8x8.drawString(0, 4, "Sending...");
-    delay(10); // bluetooth stack will go into congestion, if too many packets are sent
-  }
+void loop()
+{
+    if (deviceConnected)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
 }
